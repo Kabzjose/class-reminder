a1194a1d2751056939047f3a230cc22e88e47a0a
Joseph Ndungu <josekabz00@gmail.com>
2026-02-06 10:45:51 +0300

    enabled cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+aapp.use(cors({
+  origin: 'http://localhost:5173', // Frontend URL
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth',authRoutes)
